d7f3e24937690f67c259ca3483cdaccd4f3b74a1
Your Namemain <baiesaziz@Baiess-MacBook-Air.local>
2024-02-28 21:01:39 -0500

    main

--- 3. The Greatest Showdown.py
+++ 3. The Greatest Showdown.py
@@ -1,7 +1,19 @@
 user = float (input ("enter the number (100, 50, 30): "))
-if user == 50:
+if user > 50 > 30:
     print("100")
-elif user == 100:
-    print("100")
+
 else:
     print("100")
+
+    
+num1 = float (input ("enter the number"))
+num2 = float (input ("enter the number"))
+num3 = float (input ("enter the number"))
+if num1 >= num2 and num1 >= num3:
+    print("Largest")
+elif num2 >= num1 and num2 >= num3:
+    print("Largest")
+
+
+else:
+    print("Largest num3")
